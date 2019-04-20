a291af1ee5fbe8f2b5ff3b3f15fd8fef3caa2c0c
Sagi Gamil <Sagigamil9911@gmail.com>
2019-04-20 15:25:48 +0300

    Update main and wifi-manager

--- main/main.c
+++ main/main.c
@@ -11,30 +11,24 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 
+#include "wifi_manager_task.h"
+
+#define TAG "main"
+
+void wifi_connected(void * param)
+{
+    ESP_LOGD(TAG, "Wifi connected!");
+}
+
+void wifi_disconnected(void * param)
+{
+    ESP_LOGD(TAG, "Wifi disconnected!");
+}
 
 void app_main()
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    ESP_LOGD(TAG, "Test!");
+    wifi_manager_start_task(wifi_connected, NULL, wifi_disconnected, NULL);
 }
